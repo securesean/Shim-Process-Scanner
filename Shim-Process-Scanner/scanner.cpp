c63a7c5da198a97c2040949e206c5b11af7fff20
securesean <seaniepierce@gmail.com>
2015-07-05 00:57:35 -0400

    Init commit

---
+++ Shim-Process-Scanner/scanner.cpp
@@ -0,0 +1,25 @@
+/*
+<insert Awesome professional page header here>:
+Windows x64 Process Scanner to detect application compatability shims
+
+I applogize for:
+The commented out code
+The mixed conventions
+Ineffient code
+I did a lot of unneccary things, and this was a learning project for me - especially when dealing with undocumented structs, and programming for x64.
+The debug building fails and I can't remember how to fix it- but release will work :-)
+
+
+Want to add:
+cmd parameters for scanning a spefic process, showing debugging, verbose, etc
+continious scan?
+*/
+
+#include<stdio.h>
+
+
+int main(void){
+	printf("Hello World\n");
+
+	return 0;
+}
